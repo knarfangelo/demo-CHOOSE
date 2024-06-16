ba95d8c19714452183db7570c233c111cef64340
knarfangelo <knarf2003angelo@gmail.com>
2024-06-16 15:43:03 -0500

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { PanelBusquedaComponent } from './panel-busqueda/panel-busqueda.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PanelBusquedaComponent],
+  imports: [RouterOutlet, PanelBusquedaComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
